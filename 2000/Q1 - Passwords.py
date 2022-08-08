ee098ebe15ae6afb6b76e0b4065b4388d062dbd2
Lawson Lam <109973600+Catatria@users.noreply.github.com>
2022-08-08 09:48:51 +0800

    Update Q1 - Passwords.py

--- 2000/Q1 - Passwords.py
+++ 2000/Q1 - Passwords.py
@@ -1,21 +1,15 @@
-
 password = str(input("Input a password. ")) # Prompts user for input that will be checked
-
 # Sets variables that I use later
 count = 0
 round = 1
 length = len(password)
 length2 = int(length/2)
-
 # Performs checks to see if the input is valid
-if length <= 0:
-  print("\nPassword too short; minimum 1 character.\n")
+if 10 < length or length == 0:
+  print("\nPassword too short or long; minimum 1 character and maximum 10 characters.\n")
   exit()
 elif length == 1: # I cut corners here, if the password is one character long there cannot be any repeated characters; therefore, the password is valid
   print("\nAccepted\n")
-  exit()
-elif length >= 11:
-  print("\nPassword too long; maximum 10 characters.\n")
   exit()
 elif password.isupper() == False:
   print("\nAll characters must be uppercase.\n")
@@ -28,10 +22,9 @@ while True: # Infinite loop
     if count + 1 == length: # Checks if I have checked for repeats until the last character of the password
       if length2 == round: # Checks if the amount of characters that I am checking for repeats has reached half or more of the password, if so, the password is accepted.
         print("\nAccepted\n")
-        exit() # Program accepts and terminates as requested
+        exit()
       count = 0 # Count is reset, ready for checks with a longer string of characters
       round += 1 # Round increments
   else:
     print("\nRejected\n")
-    exit() # Program rejects and terminates as requested
-    
+    exit()
