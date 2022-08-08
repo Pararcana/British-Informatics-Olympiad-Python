db559e52dbc443479ea86f74289ceb27e41acacc
Lawson Lam <109973600+Catatria@users.noreply.github.com>
2022-08-08 18:05:02 +0800

    Update Q3 - Playing games.py

--- 2000/Q3 - Playing games.py
+++ 2000/Q3 - Playing games.py
@@ -14,10 +14,18 @@ def check(sides, check1, info1, info2, sample1, sample2): # Creates a function c
         finish = True # Makes finish true so it will print out the outcome later
         final1 = sample1.copy()
         final2 = sample2.copy() # If they aren't, the dice are saved to be printed out later
+        for i in range(sides):
+          final1[i] = str(final1[i])
+          final2[i] = str(final2[i]) # Turns items in list from int to str for concatenation
+        dice1Solution = " ".join(final1)
+        dice2Solution = " ".join(final2) # Makes format nice
+        print("\nDice 1: " + dice1Solution)
+        print("Dice 2: " + dice2Solution) # Prints out solution if it did
+        exit()
   check2.clear() # Clears check2 for the next use
 
 
-def middle(list1, list2, list3, list4, choice): # Creates a function called middle which finds a pair of numbers that adds to the pair of numbers inputted
+def middle(list1, list2, list3, list4, middle1, middle2): # Creates a function called middle which finds a pair of numbers that adds to the pair of numbers inputted
   x = int(list1)
   y = int(list2)
   z = x + y # Finds sum of pair
@@ -25,16 +33,10 @@ def middle(list1, list2, list3, list4, choice): # Creates a function called midd
     d = x + i # Finds numbers that equal the sum of that pair by adding and subtracting consecutive numbers
     f = y - i
     k = int((d+f)/2) # Finds average that may be used if sides of dice is odd
-    if choice == 0: # Appends solutions to whichever list I picked based on a variable called choice
-      middleList1.append([d,f])
-    elif choice == 1:
-      middleList1.append([d,k,f])
-    elif choice == 2:
-      middleList3.append([d,f])
-    elif choice == 3:
-      middleList3.append([d,k,f])
-    elif choice == 4:
-      middleList5.append([d,f])
+    if sides % 2 == 0: # Checks if sides is even
+      middle1.append([d,f])
+    else:
+      middle1.append([d,k,f])
   a = int(list3)
   b = int(list4)
   c = a + b
@@ -42,16 +44,10 @@ def middle(list1, list2, list3, list4, choice): # Creates a function called midd
     g = a + j
     h = b - j
     l = int((g+h)/2) # Same thing over here
-    if choice == 0:
-      middleList2.append([g,h])
-    elif choice == 1:
-      middleList2.append([g,l,h])
-    elif choice == 2:
-      middleList4.append([g,h])
-    elif choice == 3:
-      middleList4.append([g,l,h])
-    elif choice == 4:
-      middleList6.append([g,h])
+    if sides % 2 == 0:
+      middle2.append([g,h])
+    else:
+      middle2.append([g,l,h])
 
 
 def valid(example, sides, dice): # Creates a function called valid
@@ -65,6 +61,16 @@ def valid(example, sides, dice): # Creates a function called valid
       exit()
 
 
+def reset(list1, list2):
+  list1.clear()
+  list2.clear()
+
+
+def sort2(list1, list2):
+  list1.sort()
+  list2.sort()
+
+
 sides = int(input("Number of sides: ")) # Prompts user for input
 valid([sides], sides, False) # Checks input
 dice1 = input("\nDice 1: ") # Prompts user for input
@@ -73,7 +79,6 @@ valid(info1, sides, True) # Checks input
 dice2 = input("Dice 2: ") # Prompts user for input
 info2 = dice2.split(" ") # Turns input into list
 valid(info2, sides, True) # Checks input
-
 smallSolutions, bigSolutions, check1, check2, possibilities, test1, test2, test3, test4, test5, test6 = [], [], [], [], [], [], [], [], [], [], []
 middleList1, middleList2, middleList3, middleList4, middleList5, middleList6 = [], [], [], [], [], []
 finish = False # Creates and assigns a lot of variables values
@@ -84,8 +89,7 @@ for i in range(sides*2): # Turns all items in the lists from strings to integers
   else:
     info2[i-sides] = int(info2[i-sides])
     
-info1.sort()
-info2.sort() # Sorts the lists
+sort2(info1, info2) # Sorts the lists
 
 small = int(info1[0]) + int(info2[0]) # Finds the sum of the smallest numbers
 big = int(info1[sides-1]) + int(info2[sides-1]) # Finds the sum of the largest numbers
@@ -122,13 +126,9 @@ else:
       middle2 = int((possibilities[(e*2)+1][0] + possibilities[(e*2)+1][1])/2)
       test1 = [possibilities[e*2][0], middle1, possibilities[e*2][1]]
       test2 = [possibilities[(e*2)+1][0], middle2, possibilities[(e*2)+1][1]] # Appends a middle (average of both sides) to the dice
-      test1.sort()
-      test2.sort() # Sorts it
+      sort2(test1, test2) # Sorts it
       check(sides, check1, info1, info2, test1, test2) # Checks it
-    elif sides != 5: # Finds a pair of numbers that add up to the pair of numbers surrounding it
-      middle(possibilities[e*2][0], possibilities[e*2][1], possibilities[(e*2)+1][0], possibilities[(e*2)+1][1], 0)
-    elif sides == 5: # Same thing but I changed the variable choice so it will also append the average to get an extra side on the dice
-      middle(possibilities[e*2][0], possibilities[e*2][1], possibilities[(e*2)+1][0], possibilities[(e*2)+1][1], 1)
+    middle(possibilities[e*2][0], possibilities[e*2][1], possibilities[(e*2)+1][0], possibilities[(e*2)+1][1], middleList1, middleList2)
     for zi in range(len(middleList1)):
       for yj in range(len(middleList2)):
         if sides != 5:
@@ -137,15 +137,11 @@ else:
         else: # Appends to a new variable called test
           test1.append([int(possibilities[e*2][0]), int(middleList1[zi][0]), int(middleList1[zi][1]), int(middleList1[zi][2]), int(possibilities[e*2][1])])
           test2.append([int(possibilities[(e*2)+1][0]), int(middleList2[yj][0]), int(middleList2[yj][1]), int(middleList2[yj][2]), int(possibilities[(e*2)+1][1])])
-        test1[0].sort()
-        test2[0].sort()
+        sort2(test1[0], test2[0])
         if sides == 4 or sides == 5:
           check(sides, check1, info1, info2, test1[0], test2[0]) # Checks solution if sides is 4 or 5
         else:
-          if sides != 7: # Finds another pair of numbers
-            middle(test1[0][1], test1[0][2], test2[0][1], test2[0][2], 2)
-          elif sides == 7: # Finds 3 numbers
-            middle(test1[0][1], test1[0][2], test2[0][1], test2[0][2], 3)
+          middle(test1[0][1], test1[0][2], test2[0][1], test2[0][2], middleList3, middleList4)
           for xi in range(len(middleList3)):
             for wj in range(len(middleList4)):
               for k in range(4):
@@ -158,12 +154,11 @@ else:
               if sides == 7:
                 test3.append(middleList3[xi][2])
                 test4.append(middleList4[wj][2]) # Appends the extra side if the sides is 7
-              test3.sort()
-              test4.sort() # Sorts the lists
+              sort2(test3, test4) # Sorts the lists
               if sides == 6 or sides == 7: # Checks the solutions if sides are 6 and 7
                 check(sides, check1, info1, info2, test3, test4)
               else:
-                middle(test3[2], test3[3], test4[2], test4[3], 4) # Finds another pair
+                middle(test3[2], test3[3], test4[2], test4[3], middleList5, middleList6) # Finds another pair
                 for vi in range(len(middleList5)):
                   for uj in range(len(middleList6)):
                     for l in range(6):
@@ -173,28 +168,14 @@ else:
                     test5.append(middleList5[vi][1])
                     test6.append(middleList6[uj][0])
                     test6.append(middleList6[uj][1]) # Appends them to a new list
-                    test5.sort()
-                    test6.sort()
+                    sort2(test5, test6)
                     check(sides, check1, info1, info2, test5, test6) # Checks it
-                    test5.clear()
-                    test6.clear()
-                middleList5.clear()
-                middleList6.clear()
-              test3.clear()
-              test4.clear() 
-          middleList3.clear()
-          middleList4.clear()
-        test1.clear()
-        test2.clear()
-    middleList1.clear()
-    middleList2.clear() # Resets the lists to be used again
-if finish: # Checks if it found a solution
-  for i in range(sides):
-    final1[i] = str(final1[i])
-    final2[i] = str(final2[i]) # Turns items in list from int to str for concatenation
-  dice1Solution = " ".join(final1)
-  dice2Solution = " ".join(final2) # Makes format nice
-  print("\nDice 1: " + dice1Solution)
-  print("Dice 2: " + dice2Solution) # Prints out solution if it did
-else:
-  print("\nImpossible") # Else, prints impossible
+                    reset(test5, test6)
+                reset(middleList5, middleList6)
+              reset(test3, test4)
+          reset(middleList3, middleList4)
+        reset(test1, test2)
+    reset(middleList1, middleList2) # Resets the lists to be used again
+  if not finish:
+    print("\nImpossible")
+    exit()
