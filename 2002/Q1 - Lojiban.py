33d37ac3aedd2bd7d11c0611dcca1a15b54878dd
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-09-14 16:18:05 +0100

    Create Q1 - Lojiban.py

---
+++ 2002/Q1 - Lojiban.py
@@ -0,0 +1,15 @@
+denary = []
+lojbanDict = {
+  "no": "0", "pa": "1",
+  "re": "2", "ci": "3",
+  "vo": "4", "mu": "5", 
+  "xa": "6", "ze": "7",
+  "bi": "8", "so": "9"
+}
+
+lojban = input("Write a number in lojiban: ")
+
+for i in range(0, len(lojban), 2):
+  denary.append(lojbanDict[lojban[i:i+2]])
+
+print("".join(denary))
