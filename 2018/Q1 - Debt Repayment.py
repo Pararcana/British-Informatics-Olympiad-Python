813cd558e703310c04ce0045495b24e4302b38fe
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-01 09:48:48 +0100

    Update Q1 - Debt Repayment.py
    Fixed misspelling of "ceil".
    Man I hate floating points in python.

--- 2018/Q1 - Debt Repayment.py
+++ 2018/Q1 - Debt Repayment.py
@@ -1,4 +1,4 @@
-def ciel(num):
+def ceil(num):
   return num.is_integer() and num or int(num) + 1
   
 debt, total = 10000, 0
@@ -7,8 +7,8 @@ interest, repayment = input("Please input interest and repayment percentages: ")
 interest, repayment = float(interest)/100, float(repayment)/100
 
 while debt != 0:
-  debt = ciel((debt * interest) + debt)
-  total += debt <= 5000 and debt or max(ciel(debt * repayment), 5000)
-  debt -= debt <= 5000 and debt or max(ciel(debt * repayment), 5000)
+  debt = ceil((debt * interest) + debt)
+  total += debt <= 5000 and debt or max(ceil(debt * repayment), 5000)
+  debt -= debt <= 5000 and debt or max(ceil(debt * repayment), 5000)
 
 print(total/100)
