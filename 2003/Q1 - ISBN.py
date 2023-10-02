b7f92d945a1210db8e3dfb681f3c73fc0c8474ba
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-02 14:46:00 +0100

    Create Q1 - ISBN.py

---
+++ 2003/Q1 - ISBN.py
@@ -0,0 +1,19 @@
+isbn = list(input("Enter an ISBN with a missing digit: "))
+isbn.reverse()
+total = 0
+
+for i, v in enumerate(isbn):
+  try:
+    total += int(v) * (i + 1)
+  except ValueError:
+    if v == "X":
+      total += 10
+    else:
+      missingPos = i + 1
+
+for i in range(1, 12):
+  if (total + (i * missingPos)) % 11 == 0:
+    if 10 <= i <= 11:
+      print(i == 10 and "X" or 0)
+    else:
+      print(i)
