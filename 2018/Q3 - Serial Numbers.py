525904760edc24a57942109c4b553f68d5c37b10
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-10 11:31:57 +0100

    partial --> full

--- 2018/Q3 - Serial Numbers.py
+++ 2018/Q3 - Serial Numbers.py
@@ -1,3 +1,6 @@
+def match(list, serial):
+  return not any(item[0] == serial for item in list)
+
 serials, exhaustedSerials, answer = [], [], []
 digits = int(input("How many digits are in your serial number? "))
 serials.append([input("What is the serial number? "), 0])
@@ -11,7 +14,6 @@ while len(serials) != 0:
     else:
       surDigits = i == 0 and [currentSerial[2]] or [currentSerial[-3]]
     swappable = False
-    
     for j in range(len(surDigits)):
       middleCheck = [sigDigits[0], surDigits[j], sigDigits[1]]
       if not(surDigits[j] == max(middleCheck) or surDigits[j] == min(middleCheck)):
@@ -19,12 +21,10 @@ while len(serials) != 0:
     newSerial = list(currentSerial).copy()
     newSerial[i:i+2] = sigDigits[::-1]
     newSerial = "".join(newSerial)
-    if swappable and not any(item[0] == newSerial for item in exhaustedSerials): 
+    if swappable and match(exhaustedSerials, newSerial) and match(serials, newSerial): 
       serials.append([newSerial, serials[0][1] + 1])
   exhaustedSerials.append(serials.pop(0))
-  # print(len(exhaustedSerials))
 
 for item in exhaustedSerials:
   answer.append(item[1])
-
 print(max(answer))
