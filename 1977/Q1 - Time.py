8703842adf8ba0e94ac2d5b6112b58721ecde10f
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-15 13:26:23 +0100

    Create Q1 - Time.py

---
+++ 1977/Q1 - Time.py
@@ -0,0 +1,19 @@
+words = (
+  "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", 
+  "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", 
+  "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "half"
+)
+
+hours = int(input("Hours: "))
+minutes = int(input("Minutes: "))
+
+half = "to" if minutes > 30 else "past"
+if minutes > 30:
+  hours += hours == 12 and -11 or 1
+  minutes = 60 - minutes
+special = minutes not in (15, 30, 45) and "minutes " or ""
+
+if minutes == 0:
+  print(f"{words[hours]} o'clock")
+else:
+  print(f"{words[minutes]} {special}{half} {words[hours]}")
