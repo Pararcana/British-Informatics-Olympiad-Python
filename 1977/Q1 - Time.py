ec04a05cf6d31cfca9c5b809af2095931ebcbec6
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-15 13:27:06 +0100

    Update Q1 - Time.py

--- 1977/Q1 - Time.py
+++ 1977/Q1 - Time.py
@@ -1,6 +1,7 @@
 words = (
-  "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", 
-  "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", 
+  "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+  "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", 
+  "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", 
   "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "half"
 )
 
