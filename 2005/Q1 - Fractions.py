1d50e2195308bb4ffe1b5a639e0e13ad0b2a8330
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-17 08:55:14 +0100

    Update Q1 - Fractions.py

--- 2005/Q1 - Fractions.py
+++ 2005/Q1 - Fractions.py
@@ -1,7 +1,12 @@
-f, i = float(input("Decimal: ")), 0
+i, f = 0, float(input("Decimal: "))
 
 while True:
   i += 1
-  if (i/f).is_integer():
+  fraction = i/f
+  if fraction.is_integer():
     print(str(i) + "/" + str(int(i/f)))
+    break
+  # Because python is garbage at maths and 401/0.2005 gives 1999.999999... rather than 2000, which breaks the code.
+  elif str(fraction).split(".")[1][0:12] == "9" * 12: # Checks if the decimal is reoccuring.
+    print(str(i) + "/" + str(int(i/f) + 1))
     break
