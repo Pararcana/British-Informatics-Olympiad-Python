65f916acaf0213e45b9e886791fa7292af349d32
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-17 22:15:09 +0100

    Create Q1 - Fibonacci Letters.py

---
+++ 2011/Q1 - Fibonacci Letters.py
@@ -0,0 +1,10 @@
+fib1, fib2, index = input("Enter two letters and a number: ").split()
+fib1, fib2 = ord(fib1) - 64, ord(fib2) - 64
+
+fib = [fib1, fib2]
+
+for _ in range(int(index) - 2):
+  newEntry = fib[-1] + fib[-2]
+  fib.append(newEntry > 26 and newEntry - 26 or newEntry)
+
+print(chr(fib[int(index) - 1] + 64))
