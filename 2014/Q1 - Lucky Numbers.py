34562d84e77fa971a5a17b92ef6a287dea0beae4
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-20 21:00:18 +0100

    Update Q1 - Lucky Numbers.py

--- 2014/Q1 - Lucky Numbers.py
+++ 2014/Q1 - Lucky Numbers.py
@@ -1,4 +1,20 @@
+lucky = [i for i in range(1, 10005, 2)]
+n = 3
 
+for _ in range(168):
+  luckyNew = []
+  for i, v in enumerate(lucky):
+    if (i+1) % n != 0:
+      luckyNew.append(v)
+  lucky = luckyNew.copy()
+  n = lucky[lucky.index(n) + 1]
+
+num = int(input("Enter a number: "))
+
+lowest = min([v for v in lucky if v > num])
+highest = max([v for v in lucky if v < num])
+
+print(highest, lowest)
 
 # for _ in range(168):
 #   lucky = [v for v in lucky if (lucky.index(v) + 1) % n != 0]
