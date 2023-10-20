b66ed15a168f93106ec31cace7458e5b8a12928e
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-20 21:49:25 +0100

    Create Q1 - Promenade Fractions.py

---
+++ 2016/Q1 - Promenade Fractions.py
@@ -0,0 +1,12 @@
+left = [1, 0]
+right = [0, 1]
+
+promenade = input("Enter a string of left or right: ")
+
+for dir in promenade:
+  if dir == "L":
+    left = [left[0]+right[0], left[1]+right[1]]
+  else:
+    right = [left[0]+right[0], left[1]+right[1]]
+
+print(left[0] + right[0], "/", left[1] + right[1])
