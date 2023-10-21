8d429506a67ab93e98cde3408491e910dfea120a
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-21 10:16:41 +0100

    Update and rename Q1 - Cards (in progress).py to Q1 - Cards.py

---
+++ 2007/Q1 - Cards.py
@@ -0,0 +1,22 @@
+cards = [int(x) for x in input("Enter cards: ").split()]
+comb = []
+points = 0
+
+def combinations(n, lb):
+  if n != 0:
+    for i in range(lb, len(cards)):
+      buffer[n - 1] = cards[i]
+
+      if n == 1:
+        comb.append(buffer.copy())
+      combinations(n - 1, i + 1)
+
+for i in range(2, 6):
+  comb.clear()
+  buffer = [0] * i
+  combinations(i, 0)
+  for c in comb:
+    if sum(c) == 15 or (i == 2 and c[0] == c[1]):
+      points += 1
+    
+print(points)
