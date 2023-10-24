b8b7f4c45234986062842fdaadd38a79158dc9ef
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-24 21:53:30 +0100

    Update and rename Q1 - Lojiban.py to Q1 - Lojiban [E] .py

--- 2002/Q1 - Lojiban [E] .py
+++ 2002/Q1 - Lojiban [E] .py
@@ -13,3 +13,8 @@ for i in range(0, len(lojban), 2):
   denary.append(lojbanDict[lojban[i:i+2]])
 
 print("".join(denary))
+"""
+Difficulty: E
+This one was pretty simple, the use of a dictionary and extraction of each
+individual part of the string is all that is needed to solve this problem.
+"""
