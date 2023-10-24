2acf3baaa12df976cb3d7565a32bbbe394689a46
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-24 19:27:08 +0100

    Update Q1 - Anagrams.py

--- 2006/Q1 - Anagrams.py
+++ 2006/Q1 - Anagrams.py
@@ -1,7 +1,7 @@
 print(sorted(input("Word 1: ")) == sorted(input("Word 2: ")) and "Anagrams" or "Not anagrams")
 """
 Difficulty: F
-This one was so easy, i could solve it in one line. Once you realise that sorting is the
+This one was so easy, I could solve it in one line. Once you realise that sorting is the
 most efficient way to solve it, the solution comes easily. The other way of solving it; by
-counting op the number of times each letter occurs in each word isn't very hard either.
+counting of the number of times each letter occurs in each word isn't very hard either.
 """
