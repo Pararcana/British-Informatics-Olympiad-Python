10daa34a68d82f85e0e54fe3b05469f81d21131a
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-24 13:59:58 +0100

    Create Q1 - Block Palindromes (in progress).py

---
+++ 2015/Q1 - Block Palindromes (in progress).py
@@ -0,0 +1,26 @@
+def permutations(n, total, limit, parity):
+  if n != limit:
+    for i in range(1, length):
+      if length > total + 2*i and i != 0:
+        spl[n], spl[-n-1] = i, i
+        permutations(n + 1, total + 2*i, limit, parity)
+  else:
+    if parity:
+      spl[(len(spl)-1)//2] = length - (sum(spl)-spl[(len(spl)-1)//2])
+      comb.append(spl.copy())
+    else:
+      midSum = length - (sum(spl) - (spl[(len(spl)-1)//2] + spl[len(spl)//2]))
+      spl[len(spl)//2] = midSum//2
+      spl[(len(spl)-1)//2] = midSum//2
+      comb.append(spl.copy())
+
+def checkPalindrome(arr):
+  parts = []
+  for v in arr:
+    if v not in parts:
+      parts.append(v)
+  
+  compressed = []
+  for v in arr:
+    compressed.append(parts.index(v))
+  return compressed == list(reversed(compressed))
