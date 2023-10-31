b9a3d8d36c918d34ebd5637e6b49a77c83636277
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-10-31 19:20:03 +0000

    Update Q1 - Text encoder-decoder(in progress).py

--- 1995/Q1 - Text encoder-decoder(in progress).py
+++ 1995/Q1 - Text encoder-decoder(in progress).py
@@ -1,29 +1,61 @@
-#Decoder
-alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ,. ")
-key = list("I WANDERLOYSCUBFGHJKMPQTVXZ,.")
-new = list("OOANJQ,GPXXLTLUXPPQCCUUG.BEYY OY  RKDEZGGWGGVNSKKDY..ANGPPQRSSK.STTHX FFG.SSCAAJAESSMMYSDDEEO,URSOOYYG.BH..BMMBFINQWGTT,.DSZ YQCS")
-final = []
+alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ,. ")  
 
-counter = key.index(new[0])
-for i in range(len(new) - 1):
-  final.append(alpha[counter - 1])
-  counter = 29 - (key.index(new[i]) - key.index(new[i+1]))
-  if counter >= len(alpha):
-    counter -= len(alpha)
+def decoder(key, ciphertext):
+  ciphertext = [v for v in ciphertext if v in alpha]
+  final = []
 
-print("".join(final))
-########################################
-#Encoder
-alpha = list("/ABCDEFGHIJKLMNOPQRSTUVWXYZ,. ")
-key = list("I WANDERLOYSCUBFGHJKMPQTVXZ,.")
-new = list("I WANDERED LONELY AS A CLOUD THAT FLOATS ON HIGH OER DALE AND HILL WHEN ALL AT ONCE I SAW A CROWD, A FLOCK OF WANDRING DAFFODILS.")
-final = []
+  counter = key.index(ciphertext[0])
+  for i in range(len(ciphertext) - 1):
+    final.append(alpha[counter - 1])
+    counter = 29 - (key.index(ciphertext[i]) - key.index(ciphertext[i+1]))
+    if counter >= 29:
+      counter -= 29
 
-counter = 0
-for char in new:
-  counter += alpha.index(char)
-  if counter >= len(key):
-    counter -= 29
-  final.append(key[counter])
+  return "".join(final)
 
-print("".join(final))
+def encoder(key, plaintext):
+  plaintext = [v for v in plaintext if v in alpha]
+  final = []
+  
+  counter = 0
+  for char in plaintext:
+    counter += alpha.index(char) + 1
+    if counter >= 29:
+      counter -= 29
+    final.append(key[counter])
+
+  return "".join(final)
+
+def presentMenu():
+  print("""
+  1. Enter an encoding "key" string.
+  2. Encode a section of text using the current key.
+  3. Decode a section of text using the current key.
+  4. Exit.
+  """)
+  return int(input("Select your choice: "))
+
+def findKey(string):
+  alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ,. ")
+  final = []
+  string = [v for v in string if v in alpha]
+  for v in string:
+    if v in alpha:
+      final.append(v)
+      alpha.remove(v)
+  return final + alpha
+
+choice = presentMenu()
+while choice != 4:
+  if choice == 1:
+    key = findKey(input("Enter a code key: ").upper())
+  elif choice == 2:
+    print(encoder(key, input("Enter message: ").upper()))
+  elif choice == 3:
+    print(decoder(key, input("Enter message: ").upper()))
+  choice = presentMenu()
+exit()
+key = findKey("I wandered lonely as a cloud".upper())
+v = encoder(key, "I WANDERED LONELY AS A CLOUD THAT FLOATS ON HIGH O\ER DALE AND HILL WHEN ALL AT ONCE I SAW A CROWD, A\ FLOCK OF WANDRING DAFFODILS.#".upper())
+print(v)
+print(decoder(key, v.upper()))
