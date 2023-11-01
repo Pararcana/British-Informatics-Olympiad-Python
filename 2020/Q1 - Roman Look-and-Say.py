5467aa79622eac39bb3a091709a17c66d3fb97e5
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-11-01 21:37:33 +0000

    Update Q1 - Roman Look-and-Say.py

--- 2020/Q1 - Roman Look-and-Say.py
+++ 2020/Q1 - Roman Look-and-Say.py
@@ -1,9 +1,9 @@
-numerals = [
-    ["", "M", "MM", "MMM"],
-    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-]
+numerals = (
+    ("", "M", "MM", "MMM"),
+    ("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),
+    ("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
+    ("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+)
 
 def roman_splitter(roman):
   roman = "".join(roman) + " "
