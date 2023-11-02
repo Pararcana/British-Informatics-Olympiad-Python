6da667822f6b639e689f23fb901ca54043e48d37
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-11-02 12:29:11 +0000

    Update and rename Q1 - Time.py to Q1 - Time [D] .py

--- 1997/Q1 - Time [D] .py
+++ 1997/Q1 - Time [D] .py
@@ -18,3 +18,10 @@ if minutes == 0:
   print(f"{words[hours]} o'clock")
 else:
   print(f"{words[minutes]} {special}{half} {words[hours]}")
+"""
+Difficulty: D
+All in all, not that hard, and the only tedious part was writing out each number as a word, which
+you could've skipped using the num2words library. You also had to recognise special cases, such as 15
+and 30 being referred to as 'quarter' and 'half' respectively. There was also the added difficulty
+of recognising whether it was closer to, or past the hour and implementing that in the program.
+"""
