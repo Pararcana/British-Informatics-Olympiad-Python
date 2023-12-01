046d86a3b167cb75d5f20b2c5c2b8b01aab9573f
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-12-01 22:13:30 +0000

    Update Q1 - Distinct Prime Factorisation.py

--- 2012/Q1 - Distinct Prime Factorisation.py
+++ 2012/Q1 - Distinct Prime Factorisation.py
@@ -7,14 +7,9 @@ for i in range(3,  int(target**(1/2)) + 1, 2):
 if all([target % i != 0 for i in range(2, target)]):
   primes.append(target)
 
-ans = {1}
-while target != 1:
-  for v in primes:
-    if target % v == 0:
-      ans.add(v)
-      target /= v
-
+ans = set(primes)
 counter = 1
 for v in ans:
-  counter *= v
+  if target % v == 0:
+    counter *= v
 print(counter)
