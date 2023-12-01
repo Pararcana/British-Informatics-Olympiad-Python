da4aff97de0bf676d346a5343c164dd58c87f715
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-12-01 18:10:35 +0000

    Update Q1 - Digit Words.py

--- 2009/Q1 - Digit Words.py
+++ 2009/Q1 - Digit Words.py
@@ -1,35 +1,22 @@
 digitWords = ["ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN"]
 
-def lowest(arr, lb):
-  arr = [v for v in arr if v > lb]
-  try:
-    return min(arr)
-  except ValueError:
-    return None
-
-word = list(input("Enter a word: ").upper())
+word = input("Enter a word: ").upper()
 
 for digit in digitWords:
-  if set(digit).issubset(word):
-    significantChars = [v for v in word if v in digit]
+  wordFilter = [v for v in word if v in digit]
 
-    order, digitOccurences = [], []
-    for num in digit:
-      for index, char in enumerate(significantChars):
-        if char == num:
-          digitOccurences.append(index)
-      order.append(digitOccurences.copy())
-      digitOccurences.clear()
-
-    lb = -1
-    valid = True
-    for i in range(len(digit)):
-      low = lowest(order[i].copy(), lb)
-      if low is None or low <= lb:
-        valid = False
-        break
-      lb = low
-    if valid: 
-      print(digitWords.index(digit) + 1)
-      exit()
+  order = []
+  counter = 0
+  for v in digit:
+    try:
+      first = wordFilter.index(v)
+      order.append(first + counter)
+      wordFilter = [v for i, v in enumerate(wordFilter) if i > first]
+      counter += first + 1
+    except ValueError:
+      break  
+      
+  if sorted(order) == order and len(order) == len(digit):
+    print(digitWords.index(digit) + 1)
+    exit()
 print("NO")
