32dca91a506eb4cacaed862702f9fb17c973b2f8
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-12-01 18:55:58 +0000

    Update and rename Q1 - Anagram Numbers.py to Q1 - Anagram Numbers [D] .py

--- 2010/Q1 - Anagram Numbers [D] .py
+++ 2010/Q1 - Anagram Numbers [D] .py
@@ -9,3 +9,8 @@ if ans:
   print(" ".join(ans))
 else:
   print("NO")
+"""
+Difficulty: D
+Really not that difficult, the thing you should remember with anagrams, is that
+you can sort them and see if they give the same result.
+"""
