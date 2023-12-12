e1825ddd9fbc993adc4739e59c1adfee688cbda5
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2023-12-12 07:24:08 +0000

    Update Q3 - Playing Games [X] .py

--- 2000/Q3 - Playing Games [X] .py
+++ 2000/Q3 - Playing Games [X] .py
@@ -1,12 +1,10 @@
 die1 = [sorted([int(x) for x in input("Dice 1: ").split()]), sorted([int(x) for x in input("Dice 2: ").split()])]
 dicePerms = sorted([i + j for i in die1[0] for j in die1[1]])
 diceCount = [dicePerms.count(v) for v in range(2, 17)]
-setPerms = set(dicePerms)
 sides = len(die1[0])
-small = min(dicePerms)
-big = max(dicePerms)
+small, big = min(dicePerms), max(dicePerms)
 
-def check(arr): # 13s for 1,000,000
+def check(arr):
   testPerms = sorted([i + j for i in arr[0] for j in arr[1]])
   if dicePerms == testPerms and arr[0] not in [die1[0], die1[1]]:
     final1 = [str(x) for x in arr[0].copy()]
@@ -15,7 +13,7 @@ def check(arr): # 13s for 1,000,000
     print("Dice 2: " + " ".join(final2)) 
     exit()
 
-def softCheck(arr): # 17s for 1,000,000; ~750,00 in 1s
+def softCheck(arr):
   testPerms = []
   testPerms = [i + j for i in arr[0] for j in arr[1]]
   testCount = [testPerms.count(v) for v in range(2, 17)]
@@ -35,7 +33,7 @@ def pairs(arr, target=None):
     for i in range(1, 11):
       for j in range(i, 11):
         if i+j == target:
-          yield (i, j)
+          yield [[i], [j]]
   else:
     x = prune(arr)
     arr[0].pop(-1)
@@ -44,64 +42,28 @@ def pairs(arr, target=None):
     y = prune(arr)
     arr[0].pop(-1)
     arr.reverse()
-    
+
     for i in range(arr[0][-1], 11):
       if i in x:
         for j in range(arr[1][-1], 11):
           if j in y and small <= i + j <= big:
             yield (i, j)
 
+def step(testDie, n=1):
+  if n == sides:
+    check(testDie)
+    
+  else:
+    for v in pairs(testDie):
+      for i in range(2):
+        
+        while len(testDie[i]) > n:
+          testDie[i].pop(-1)
+        testDie[i].append(v[i])
+        
+      step(testDie, n + 1)
 
-if sides == 1:
-  for v in pairs(None, target=small):
-    check([[v[0]], [v[1]]])
-elif sides == 2:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      check([[v[0], x[0]], [v[1], x[1]]])
-elif sides == 3:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        check([[v[0], x[0], y[0]], [v[1], x[1], y[1]]])
-elif sides == 4:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        for z in pairs([[v[0], x[0], y[0]], [v[1], x[1], y[1]]]):
-          check([[v[0], x[0], y[0], z[0]], [v[1], x[1], y[1], z[1]]])
-elif sides == 5:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        for z in pairs([[v[0], x[0], y[0]], [v[1], x[1], y[1]]]):
-          for i in pairs([[v[0], x[0], y[0], z[0]], [v[1], x[1], y[1], z[1]]]):
-            check([[v[0], x[0], y[0], z[0], i[0]], [v[1], x[1], y[1], z[1], i[1]]])
-elif sides == 6:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        for z in pairs([[v[0], x[0], y[0]], [v[1], x[1], y[1]]]):
-          for i in pairs([[v[0], x[0], y[0], z[0]], [v[1], x[1], y[1], z[1]]]):
-            for j in pairs([[v[0], x[0], y[0], z[0], i[0]], [v[1], x[1], y[1], z[1], i[1]]]):
-              check([[v[0], x[0], y[0], z[0], i[0], j[0]], [v[1], x[1], y[1], z[1], i[1], j[1]]])
-elif sides == 7:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        for z in pairs([[v[0], x[0], y[0]], [v[1], x[1], y[1]]]):
-          for i in pairs([[v[0], x[0], y[0], z[0]], [v[1], x[1], y[1], z[1]]]):
-            for j in pairs([[v[0], x[0], y[0], z[0], i[0]], [v[1], x[1], y[1], z[1], i[1]]]):
-              for n in pairs([[v[0], x[0], y[0], z[0], i[0], j[0]], [v[1], x[1], y[1], z[1], i[1], j[1]]]):
-                check([[v[0], x[0], y[0], z[0], i[0], j[0], n[0]], [v[1], x[1], y[1], z[1], i[1], j[1], n[1]]])
-elif sides == 8:
-  for v in pairs(None, target=small):
-    for x in pairs([[v[0]], [v[1]]]):
-      for y in pairs([[v[0], x[0]], [v[1], x[1]]]):
-        for z in pairs([[v[0], x[0], y[0]], [v[1], x[1], y[1]]]):
-          for i in pairs([[v[0], x[0], y[0], z[0]], [v[1], x[1], y[1], z[1]]]):
-            for j in pairs([[v[0], x[0], y[0], z[0], i[0]], [v[1], x[1], y[1], z[1], i[1]]]):
-              for n in pairs([[v[0], x[0], y[0], z[0], i[0], j[0]], [v[1], x[1], y[1], z[1], i[1], j[1]]]):
-                for m in pairs([[v[0], x[0], y[0], z[0], i[0], j[0], n[0]], [v[1], x[1], y[1], z[1], i[1], j[1], n[1]]]):
-                  check([[v[0], x[0], y[0], z[0], i[0], j[0], n[0], m[0]], [v[1], x[1], y[1], z[1], i[1], j[1], n[1], m[1]]])
+for v in pairs(None, target=small):
+  step(v)
+  
 print("Impossible")
