0dfb55792dedd5ead4163877dd26f5e0a422d561
Lawson Lam <109973600+Pararcana@users.noreply.github.com>
2024-01-18 13:02:58 +0000

    Update Q2 - Ants [B] .py

--- 2000/Q2 - Ants [B] .py
+++ 2000/Q2 - Ants [B] .py
@@ -3,20 +3,20 @@ class Ant:
     self.coords = int(coords[0]), int(coords[1]), coords[2]
 
   def __str__(self):
-    return " ".join(str(v) for v in self.coords) if self.coords[0] else "Removed"
+    return " ".join(str(v) for v in self.coords) if self.coords else "Removed"
 
   def move(self):
-    x, y, dir = self.coords
-    if 0 < x < 12 and 0 < y < 12:
+    if self.coords:
+      x, y, dir = self.coords
       incre = 1 if faces.index(dir) < 2 else -1
       if faces.index(dir) % 2: x += incre 
       else: y += incre
         
-      if not(0 < x < 12 and 0 < y < 12): x, empty = 0, 0
+      if not(0 < x < 12 and 0 < y < 12): self.coords = False
       else:
         empty = 1 if grid[-y + 11][x - 1] == "." else -1
         grid[-y + 11][x - 1] = "*" if empty == 1 else "."
-      self.coords = x, y, faces[(faces.index(dir) + empty) % 4]
+        self.coords = x, y, faces[(faces.index(dir) + empty) % 4]
 
 position1 = input("Input ant 1's co-ordinates and direction: ").upper().split(" ")
 position2 = input("Input ant 2's co-ordinates and direction: ").upper().split(" ")
@@ -28,6 +28,7 @@ while (moves := int(input("\nNumber of moves: "))) != -1:
   for _ in range(moves): ant1.move(); ant2.move()
   for row in grid: print(*row)
   print(ant1); print(ant2)
+
 """
 Difficulty: B
 This questions involves the use of classes, so you can define how both ants move according to
